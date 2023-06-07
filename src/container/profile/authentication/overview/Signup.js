b03a1b82a1b1285054bc4c18d3e5fec564fa16ae
yeonjae97 <yeonjae978@gmail.com>
2023-06-07 18:34:09 +0900

    회원가입 폼 수정, 회원가입 페이지에서 회원 데이터 삽입 완료, 회원가입 유효성 검증, 주소 api, 이메일 인증 구현 진행 중

--- src/container/profile/authentication/overview/Signup.js
+++ src/container/profile/authentication/overview/Signup.js
@@ -1,37 +1,110 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ReactSVG } from 'react-svg';
-import { Row, Col, Form, Input, Button } from 'antd';
-import UilFacebook from '@iconscout/react-unicons/icons/uil-facebook-f';
-import UilTwitter from '@iconscout/react-unicons/icons/uil-twitter';
-import UilGithub from '@iconscout/react-unicons/icons/uil-github';
-
+import { Row, Col, Form, Input, Button} from 'antd';
 import { useDispatch } from 'react-redux';
+// import { Option } from 'antd/lib/mentions';
 import { AuthFormWrap } from './style';
-import { Checkbox } from '../../../../components/checkbox/checkbox';
+// import { Checkbox, CheckboxGroup } from '../../../../components/checkbox/checkbox';
 import { register } from '../../../../redux/authentication/actionCreator';
+// import FormItemLabel from 'antd/es/form/FormItemLabel';
 
 function SignUp() {
   const dispatch = useDispatch();
 
-  const [state, setState] = useState({
-    values: null,
-    checked: null,
+  // 지금 있는 값 또는 체크를 한번에 관리하겠다는 의도
+  
+  // 성별 관리
+  // const [isGender, setIsGender] = useState({
+    //   male: false,
+    //   female: false,
+  // });
+  // // 회원 아이디
+  // const [userId, setUserId] = useState('');
+  // // 비밀번호
+  // const [password, setPassword] = useState('');
+  // // 닉네임
+  // const [nickName, setNickName] = useState('');
+  // // 이름
+  // const [userName, setUserName] = useState('');
+  // 생년월일
+  const now = new Date();
+  const [birth, setBirth] = useState({
+    year: '2022',
+    month: '01',
+    day: '01',
   });
-  const handleSubmit = (values) => {
-    dispatch(register(values));
+  const years = [];
+  const months = [];
+  const days = [];
+  // 년
+  for (let y = 1930; y < now.getFullYear(); y += 1) {
+    years.push(y);
+  }
+  // 월
+  for (let m = 1; m <= 12; m += 1) {
+    if (m < 10) {
+      months.push(`0${m.toString()}`);
+    } else {
+      months.push(m.toString());
+    }
+  }
+  // 일
+  const date = new Date(birth.year, birth.month, 0).getDate();
+  for (let d = 0; d <= date; d += 1) {
+    if (d < 10) {
+      days.push(`0${d.toString()}`);
+    } else {
+      days.push(d.toString());
+    }
+  }
+  // 성별
+  const [gender, setGender] = useState({
+    selectList: ['남자', '여자'],
+    selectValue: '남자',
+  });
+  
+  // 우편번호 검색시 팝업창 띄울 여부
+  // const [IsPopUpOpen, setIsPopUpOpen] = useState(false);
+
+  // const handlePopUp = () => {
+  //   setIsPopUpOpen(true);
+  // }
+
+
+  const handleGender = (e) => {
+    console.log(e.target.value);
+    setGender({
+      selectValue: e.target.value,
+    });
+  };
+  // Submit 했을 시에 값 등록
+  const handleSubmit = (e) => {
+    const { userId, password, nickName, userName, email, address} = e
+    // const objValue = useState({
+    //   userId, password, nickName, userName, email, gender, birth
+    // });
+    dispatch(register({
+      userId,
+      password,
+      nickName,
+      userName,
+      email,
+      gender: gender.selectValue,
+      birth: birth.year + birth.month + birth.day,
+      address
+    }));
   };
 
-  const onChange = (checked) => {
-    setState({ ...state, checked });
-  };
+  // const onChange = (checked) => {
+  //   setState({ ...state, checked });
+  // };
 
   return (
     <Row justify="center">
       <Col xxl={6} xl={8} md={12} sm={18} xs={24}>
         <AuthFormWrap>
           <div className="ninjadash-authentication-top">
-            <h2 className="ninjadash-authentication-top__title">Sign Up HexaDash</h2>
+            <h2 className="ninjadash-authentication-top__title">회원가입</h2>
           </div>
           <div className="ninjadash-authentication-content">
             <Form name="register" onFinish={handleSubmit} layout="vertical">
@@ -56,11 +129,10 @@ function SignUp() {
                   },
                 ]}
               >
-                <Input placeholder="root" />
+                <Input placeholder="아이디를 입력하세요" />
               </Form.Item>
               <Form.Item
                 name="password"
-                // rules={[{ message: '대소문자, 특수문자, 숫자 포함 8 ~ 20자리여야 합니다', required: true }]}
                 rules={[
                   {
                     type: 'password',
@@ -78,74 +150,104 @@ function SignUp() {
                   },
                 ]}
                 // rules={[{ message: passwordMessage, required: true }]}
-                initialValue="123456"
                 label="비밀번호"
                 // onChange={onChangePassword}
               >
                 <Input.Password placeholder="비밀번호를 입력해주세요." />
               </Form.Item>
-              <Form.Item label="이름" name="name" rules={[{ required: true, message: '이름을 입력해주세요.' }]}>
-                <Input placeholder="Full name" />
+              <Form.Item label="이름" name="userName" rules={[{ required: true, message: '이름을 입력해주세요.' }]}>
+                <Input placeholder="본인 이름을 입력하세요" />
               </Form.Item>
               <Form.Item label="닉네임" name="nickName" rules={[{ required: true, message: '닉네임을 입력해주세요.' }]}>
-                <Input placeholder="Full name" />
+                <Input placeholder="닉네임을 입력하세요" />
               </Form.Item>
-              <Form.Item
-                label="성별"
-                name="gender"
-                rules={[{ required: true, message: 'Please input your Full name!' }]}
-              >
-                <Input placeholder="Full name" />
+              <Form.Item label="성별" name="gender">
+                  <Input
+                    type='radio'
+                    id="male"
+                    value="남자"
+                    checked={gender.selectValue === '남자'}
+                    onChange={handleGender}
+                  />
+                  <Input
+                    type='radio'
+                    id="female"
+                    value="여자"
+                    checked={gender.selectValue === '여자'}
+                    onChange={handleGender}
+                  />
               </Form.Item>
+              
               <Form.Item
                 label="생년월일"
+                className="birth-form"
                 name="birth"
-                rules={[{ required: true, message: 'Please input your Full name!' }]}
+                rules={[{ required: false, message: '생년월일을 선택하세요.' }]}
               >
-                <Input placeholder="Full name" />
+                <select
+                  className="birth1"
+                  value={birth.year}
+                  onChange={(e) => setBirth({ ...birth, year: e.target.value })}
+                >
+                  {birth &&
+                    years.map((item) => (
+                      <option value={item} key={item}>
+                        {item}
+                      </option>
+                    ))}
+                </select>
+                년
+                <select
+                  value={birth.month}
+                  onChange={(e) => setBirth({ ...birth, month: e.target.value })}
+                >
+                  {birth &&
+                    months.map((item) => (
+                      <option value={item} key={item}>
+                        {item}
+                      </option>
+                    ))}
+                </select>
+                월
+                <select value={birth.day} onChange={(e) => setBirth({ ...birth, day: e.target.value })}>
+                  {days &&
+                    days.map((item) => (
+                      <option value={item} key={item}>
+                        {item}
+                      </option>
+                    ))}
+                </select>
+                일
+              </Form.Item>
+              {/* <Form.Item label="우편번호" rules={[{ required: true, message: '주소를 입력해주세요.' }]}>
+                <Input placeholder="주소를 입력해주세요." />
+                <Button type='button' onClick={handlePopUp}></Button>
+              </Form.Item> */}
+              <Form.Item label="주소" name="address" rules={[{ required: true, message: '주소를 입력해주세요.' }]}>
+                <Input placeholder="주소를 입력해주세요." />
               </Form.Item>
               <Form.Item
                 label="이메일"
                 name="email"
-                rules={[{ required: true, message: 'Please input your Full name!' }]}
+                rules={[
+                  {
+                    type: 'email',
+                    message: '이메일을 입력하세요',
+                    required: true,
+                  },
+                  {
+                    pattern: /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+                    message: '올바른 이메일 형식을 입력하세요',
+                  },
+                ]}
               >
-                <Input placeholder="Full name" />
+                <Input placeholder="이메일을 입력하세요." />
               </Form.Item>
-              <div className="ninjadash-auth-extra-links">
-                <Checkbox onChange={onChange} checked={state.checked}>
-                  Creating an account means you’re okay with our Terms of Service and Privacy Policy
-                </Checkbox>
-              </div>
               <Form.Item>
                 <Button className="btn-create" htmlType="submit" type="primary" size="large">
-                  Create Account
+                  회원 생성
                 </Button>
               </Form.Item>
-              <p className="ninjadash-form-divider">
-                <span>Or</span>
-              </p>
-              <ul className="ninjadash-social-login">
-                <li>
-                  <Link className="google-social" to="#">
-                    <ReactSVG src={require(`../../../../static/img/icon/google-plus.svg`).default} />
-                  </Link>
-                </li>
-                <li>
-                  <Link className="facebook-social" to="#">
-                    <UilFacebook />
-                  </Link>
-                </li>
-                <li>
-                  <Link className="twitter-social" to="#">
-                    <UilTwitter />
-                  </Link>
-                </li>
-                <li>
-                  <Link className="github-social" to="#">
-                    <UilGithub />
-                  </Link>
-                </li>
-              </ul>
             </Form>
           </div>
           <div className="ninjadash-authentication-bottom">
