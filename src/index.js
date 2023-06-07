1cd2f578ef39c5d92e4cb258a9dc148a9e2f3c84
이시화 <tlghk9898@gmail.com>
2023-06-07 18:20:33 +0900

    eslintrx - 오류 관련 추가항목 index.js - strictMode 삭제 plogging  현재 위치 띄우기성공 index.html -  tmap api추가

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
