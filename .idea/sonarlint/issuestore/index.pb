b9e5847fa42c4849285539398de8b297ce0b52bd
developerjini <i5veagl@gmail.com>
2023-06-08 18:11:35 +0900

    커뮤니티 게시판 리스트 조회 템플릿 예제 테스트

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,23 @@
+
+P
+ src/container/pages/challenge.js,3\e\3e2475a6fea93e707d8058995e939a6479882a97
+V
+&src/container/pages/ChallengeCreate.js,7\b\7bab747cda093438c0b6d8455c70868ee1464a76
+S
+#src/components/checkbox/checkbox.js,6\9\692102ac7f5a68163950adf603eeaf9d538e00f6
+9
+	.eslintrc,4\b\4be5f5e3cea171c304090be92469b2f2cb51039a
+;
+.prettierrc,2\0\20137a9e94744624390b6748bc96df5ca1579b71
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+I
+src/routes/admin/pages.js,7\6\7637fbcaf0ffd5bd7969d7954ddc0e01e46310e4
+P
+ src/components/cards/BlogCard.js,3\7\3722c5e9ef9c6391e5cb55cb6c2bd2fa78da0d02
+L
+src/container/pages/board.js,5\5\55829b996eb3f6f20abf9871c71a5705b3ed67c0
+H
+src/static/css/style.css,5\2\5278550501a4adda3a484630a3bd48f734aba3b4
+M
+src/demoData/sampleCards.json,c\b\cb692dc9beb651ba176876d4799a45681d8c4f6a
--- src/container/pages/board.js
+++ src/container/pages/board.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Col, Row } from 'antd';
 import { PageHeader } from '../../components/page-headers/page-headers';
 import { Main } from '../styled';
-import { Cards } from '../../components/cards/frame/cards-frame';
+import cardData from '../../demoData/sampleCards.json';
+import BlogCard from '../../components/cards/BlogCard';
 
+const { BlogCardData } = cardData;
 function Board() {
   const boardPage = [
     {
@@ -15,12 +17,23 @@ function Board() {
     <>
       <PageHeader className="ninjadash-page-header-main" title="커뮤니티" routes={boardPage} />
       <Main>
-        <Row gutter={25}>
-          <Col sm={24} xs={24}>
-            <Cards headless>
-              <h3>커뮤니티 페이지</h3>
-            </Cards>
-          </Col>
+        <Row gutter={1} className="mt-sm-10">
+          {BlogCardData.slice(1, 2).map((blog) => {
+            return (
+              <Col key={blog.id} xxl={4} xl={4} sm={4} xs={4}>
+                <BlogCard item={blog} theme="style-2" />
+              </Col>
+            );
+          })}
+        </Row>
+        <Row gutter={1} className="mt-sm-10">
+          {BlogCardData.slice(1, 2).map((blog) => {
+            return (
+              <Col key={blog.id} xxl={4} xl={4} sm={4} xs={4}>
+                <BlogCard item={blog} theme="style-2" />
+              </Col>
+            );
+          })}
         </Row>
       </Main>
     </>
