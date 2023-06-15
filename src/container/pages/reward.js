0c08c6f085f5de9bc6ca1e28acab6fec2ebe6f6b
flatjava <wodnjsdl01@gmail.com>
2023-06-15 17:03:58 +0900

    rewardList, pageStyle.scss 파일 생성

--- src/container/pages/reward.js
+++ src/container/pages/reward.js
@@ -3,28 +3,49 @@ import { Col, Row } from 'antd';
 import { PageHeader } from '../../components/page-headers/page-headers';
 import { Main } from '../styled';
 import { Cards } from '../../components/cards/frame/cards-frame';
+import FontAwesome from 'react-fontawesome';
+import { useEffect, useState } from 'react';
+import { DataService } from '../../config/dataService/dataService';
+import RewardList from './rewardList';
+import ReactDOM from 'react-dom';
 
 function Reward() {
-  const rewardPage = [
-    {
-      path: '',
-      breadcrumbName: '리워드',
-    },
-  ];
-  return (
-    <>
-      <PageHeader className="ninjadash-page-header-main" title="리워드" routes={rewardPage} />
-      <Main>
-        <Row gutter={25}>
-          <Col sm={24} xs={24}>
-            <Cards headless>
-              <h3>리워드 페이지</h3>
-            </Cards>
-          </Col>
-        </Row>
-      </Main>
-    </>
-  );
-}
+  const [list, setList] = useState([]);
+  const [name, setName] = useState([]);
+  const [detail, setDetail] = useState([]);
 
+  useEffect(() => {
+    DataService.get(`/reward/list`).then(function (response) {
+      setList(response.data.data);
+      console.log('reward List Test : ');
+      console.log(response.data.data);
+    });
+  }, [name, detail]);
+
+  function Reward() {
+    const rewardPage = [
+      {
+        path: '',
+        breadcrumbName: '리워드',
+      },
+    ];
+    return (
+      <>
+        <PageHeader className="ninjadash-page-header-main" title="리워드" routes={rewardPage} />
+        <Main>
+          <Row gutter={25}>
+            <Col sm={24} xs={24}>
+              <Cards headless>
+                {list.map((data) => (
+                  <RewardList name={data} />
+                ))}
+                <h3>리워드 페이지</h3>
+              </Cards>
+            </Col>
+          </Row>
+        </Main>
+      </>
+    );
+  }
+}
 export default Reward;
---
+++ src/container/pages/rewardList.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function RewardList() {
+  return (
+    <>
+      <div className="reward-title">
+        <h4>리워드 List Test</h4>
+      </div>
+    </>
+  );
+}
+
+export default React.memo(RewardList);
