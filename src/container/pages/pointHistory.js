9a4e233b299bd0c8eb624bd11f7ceaa9c179a5fc
flatjava <wodnjsdl01@gmail.com>
2023-06-16 13:02:58 +0900

    quill, redux-actions, redux-saga, react-rte add시 추가된 파일, pointHistory 수정 및 추가

--- src/container/pages/pointHistory.js
+++ src/container/pages/pointHistory.js
@@ -1,34 +1,36 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { DataService } from '../../config/dataService/dataService';
-import { Button } from '../../components/buttons/buttons';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { DataService } from "../../config/dataService/dataService";
+import { Button } from "../../components/buttons/buttons";
+import { response } from "yarn/lib/cli";
+
 
 function pointHistory() {
-  // const onSubmit = () => {
-  //   axios
-  //     .post(
-  //       '/history',
-  //       JSON.stringify({
-  //         memberNo: '',
-  //         type: '',
-  //         point: '',
-  //         rewardNo: '',
-  //       }),
-  //     )
-  //     .then((res) => console.log(res));
-  // };
-  const createPointhistory = () => {
-    DataService.post('/history/donation', {
-      memberNo: '1',
-      type: 'donation',
-      point: '-1000',
-      rewardNo: '1',
+  const createDonation = () => {
+    DataService.post("/history/donation", {
+      type: "donation",
+      point: -1000,
+      rewardNo: 1
+    }).then((response) => console.log("test", response));
+  };
+
+  const createProduct = () => {
+    DataService.post("/history/product", {
+      type: "product",
+      point: -8000,
+      rewardNo: 9
     }).then((response) => console.log(response));
   };
   return (
-    <Button size="default" type="primary" key="submit" onClick={createPointhistory}>
-      기부하기
-    </Button>
+    <div>
+      <Button size="default" type="primary" key="submit" onClick={createDonation}>
+        기부하기
+      </Button>
+      <Button size="default" type="primary" key="submit" onClick={createProduct}>
+        랜덤박스 신청
+      </Button>
+    </div>
   );
 }
+
 export default pointHistory;
