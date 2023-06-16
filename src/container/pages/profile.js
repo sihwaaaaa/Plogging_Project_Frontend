a15dd7856fde4a861a2f8266bbd8b579b01f895b
yeonjae97 <yeonjae978@gmail.com>
2023-06-16 18:23:05 +0900

    프로필 스타일 및 화면 구성 추가

--- src/container/pages/profile.js
+++ src/container/pages/profile.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import { Col, Row } from 'antd';
+import { Col, Row, Tabs } from 'antd';
 import { PageHeader } from '../../components/page-headers/page-headers';
 import { Main } from '../styled';
 import { Cards } from '../../components/cards/frame/cards-frame';
+import profileStyle from '../../../src/static/css/profileStyle.scss';
+import UserCards from './overview/UserCard';
+import CategoryComponent from './overview/CategoryComponent';
+import { useState } from 'react';
+
+    const BottomContent = () => (
+      <Cards headless className="bottom-content">
+        <Tabs
+          type="card"
+          items={new Array(3).fill(null).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of Tab Pane ${id}`,
+            };
+          })}
+        />
+    </Cards>
+    );
+  
 
 function Profile() {
+  
+    // 이 카테고리의 타입은 객체 타입
+
   const profilePage = [
     {
       path: '',
-      breadcrumbName: '프로필',
+      breadcrumbName: '',
     },
   ];
   return (
     <>
-      <PageHeader className="ninjadash-page-header-main" title="프로필" routes={profilePage} />
-      <Main>
+      <PageHeader className="ninjadash-page-header-main" title="" routes={profilePage} />
+      <Main className='profile-component'>
         <Row gutter={25}>
           <Col sm={24} xs={24}>
-            <Cards headless>
-              <h3>프로필 페이지</h3>
-            </Cards>
+            <UserCards />
+            <CategoryComponent  />
+            <BottomContent />
           </Col>
         </Row>
       </Main>
