a8371347fe05e87b46246f29a106869bd30bd43a
flatjava <wodnjsdl01@gmail.com>
2023-06-19 13:15:22 +0900

    프론트 화면 배치 진행 중

--- src/container/pages/reward.js
+++ src/container/pages/reward.js
@@ -1,33 +1,39 @@
 import React, { Suspense } from "react";
-import { useEffect, useState } from 'react';
-import { DataService } from '../../config/dataService/dataService';
+import { useEffect, useState } from "react";
+import { DataService } from "../../config/dataService/dataService";
 // import "../../components/cards/Style"
 import { KnowledgebaseTopWrap } from "./knowledgeBase/style";
 import { Form, Input } from "antd";
 import { Button } from "../../components/buttons/buttons";
 import FontAwesome from "react-fontawesome";
-import '../../static/css/rewardPageStyle.scss'
+import "../../static/css/rewardPageStyle.scss";
 import PointHistory from "./pointHistory";
 
 const Reward = () => {
   const [rewardList, setRewardList] = useState([]);
 
   useEffect(() => {
-    DataService.get(`/reward/list`).then(function (response) {
+    DataService.get(`/reward/list`).then(function(response) {
       setRewardList(response.data);
-      console.log('reward List Test : ');
+      console.log("reward List Test : ");
       console.log(response.data);
     });
   }, []);
   return (
     <>
       <div className="rewardContainer">
-        <div className="searchWrapper">
-          <div className="searchText">
-            <PointHistory />
-            <h3>함께할 플로거를</h3>
-            <h3>찾아보세요!</h3>
-          </div>
+        <div className="rewardHeader1">
+
+        </div>
+        <div className="rewardHeader2">
+          <KnowledgebaseTopWrap>
+            <div className="ninjadash-knowledgetop">
+              <div className="rewardinfoText">
+                <h3>함께할 플로거를</h3>
+                <h3>찾아보세요!</h3>
+              </div>
+            </div>
+          </KnowledgebaseTopWrap>
         </div>
       </div>
     </>
@@ -35,11 +41,19 @@ const Reward = () => {
 };
 export default Reward;
 
-{/*<PointHistory />*/}
-{/*{rewardList.map((res) => (*/}
-{/*  <div>*/}
-{/*    <span>{res.name}</span>*/}
-{/*    <span>{res.detail}</span>*/}
-{/*    <span>{res.type}</span>*/}
-{/*  </div>*/}
-{/*))}*/}
+{/*<PointHistory />*/
+}
+{/*{rewardList.map((res) => (*/
+}
+{/*  <div>*/
+}
+{/*    <span>{res.name}</span>*/
+}
+{/*    <span>{res.detail}</span>*/
+}
+{/*    <span>{res.type}</span>*/
+}
+{/*  </div>*/
+}
+{/*))}*/
+}
