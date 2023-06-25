52fb6c118b52c87aaebccd4ff0d9faccaf4f18eb
yeonjae97 <yeonjae978@gmail.com>
2023-06-26 08:29:33 +0900

    회원가입 오류 수정 및 아이디 찾기 폼 준비, 소셜 로그인 데이터 등록은 완료

--- src/container/profile/authentication/overview/SocialLogin.js
+++ src/container/profile/authentication/overview/SocialLogin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import actions from '../../../../redux/authentication/actions';
+import Cookies from 'js-cookie';
 
 function SocialLogin(props) {
   // const history = useNavigate();
@@ -14,10 +15,27 @@ function SocialLogin(props) {
     return params.get(name);
   };
   const token = getUrlParameter('token');
+  const memberNo = getUrlParameter('memberNo');
+  const userId = getUrlParameter('userId');
+  const nickName = getUrlParameter('nickName');
+  const userName = getUrlParameter('userName');
+  const gender = getUrlParameter('gender');
+  const regDate = getUrlParameter('regDate');
   console.log(`토큰 파싱: ${token}`);
+
   if (token) {
+    alert("소셜 로그인을 완료하였습니다.");
     console.log(`로컬 스토리지에 토큰 저장${token}`);
     localStorage.setItem('ACCESS_TOKEN', token);
+    Cookies.set('ACCESS_TOKEN', token);
+    Cookies.set('logedIn', true);
+    Cookies.set('userId', userId);
+    Cookies.set('memberNo', memberNo);
+    Cookies.set('nickName', nickName);
+    Cookies.set('userName', userName);
+    Cookies.set('gender', gender);
+    // Cookies.set('birth', res.data.data.birth);
+    Cookies.set('regDate', regDate);
     dispatch(actions.loginSuccess(true));
     return (
       <Navigate
